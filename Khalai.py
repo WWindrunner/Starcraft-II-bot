943c51792c3a4f5b02ef4387f96b740f6e9dcf2f
Maopu Xu <xumaopu233@g.ucla.edu>
2024-04-02 20:22:15 -0700

    updated code

--- Khalai.py
+++ Khalai.py
@@ -1,73 +1,84 @@
 import sc2
-# run_game启动游戏 指定游戏参数
-# maps选地图 Race选种族 Difficulty选难度
-from sc2 import run_game, maps, Race, Difficulty, UnitTypeId, AbilityId
-# AI和内置电脑
-from sc2.player import Bot, Computer, Human
-from sc2.constants import *
+from sc2 import maps
+# , Race, Difficulty, UnitTypeId, AbilityId
+from sc2.player import Bot, Computer
+from sc2.main import run_game
+from sc2.data import Race, Difficulty
+from sc2.bot_ai import BotAI
+from sc2.ids.ability_id import AbilityId
+from sc2.ids.buff_id import BuffId
+from sc2.ids.unit_typeid import UnitTypeId
+
 import random
 
 
-class SigmaStar(sc2.BotAI):
+class KhalaiStar(BotAI):
     async def on_step(self, iteration: int):
+        if iteration == 0:
+            await self.chat_send("(glhf)")
+
+        # Attack with all workers if we don't have any nexuses left
+        if not self.townhalls.ready:
+            for worker in self.workers:
+                worker.attack(self.enemy_start_locations[0])
+            return
+        nexus = self.townhalls.ready.random
+
         await self.distribute_workers()
-        for nexus in self.units(NEXUS).ready:
-            await self.handle_chronoboost(nexus)
-        await self.build_workers()
-        if self.supply_left < 7 and not self.supply_cap == 200:
-            await self.build_pylons()
-        await self.build_assimilators()
-        if self.minerals > 400 and self.known_enemy_units.exclude_type({OVERLORD, DRONE, PROBE, SCV}).amount == 0:
+        # for nexus in self.units(NEXUS).ready:
+        #     await self.handle_chronoboost(nexus)
+        await self.build_workers(nexus)
+
+        if not self.supply_cap >= 200:
+            await self.build_pylons(nexus)
+
+        # await self.build_assimilators()
+
+        if self.minerals > 400 \
+                and self.townhalls.amount < 3 \
+                and self.enemy_units.exclude_type({UnitTypeId.OVERLORD, UnitTypeId.DRONE, UnitTypeId.PROBE, UnitTypeId.SCV}).amount == 0:
             await self.expand()
-        if self.minerals > 150 and self.units(NEXUS).amount > 1:
+
+        if self.minerals > 150 and self.townhalls.ready.amount > 1:
             await self.offensive_force_buildings()
-        if self.supply_used < 40 or self.units(NEXUS).amount > 1:
+        if self.supply_used < 40 or self.townhalls.ready.amount > 1:
             await self.build_offensive_force()
-        if self.supply_army > 40:
-            await self.handle_upgrades()
+        # if self.supply_army > 40:
+        #     await self.handle_upgrades()
+
         await self.attack()
 
-    async def build_workers(self):
-        num = 0
-        for n in self.units(NEXUS).ready:
-            num += n.ideal_harvesters
-        if len(self.units(ASSIMILATOR).ready)*3 + num > len(self.units(PROBE)) and self.units(PROBE).amount < 70:
-            for nexus in self.units(NEXUS).ready.idle:
-                if self.can_afford(PROBE):
-                     await self.do(nexus.train(PROBE))
+    async def build_workers(self, nexus):
+        if self.supply_workers + self.already_pending(UnitTypeId.PROBE) < self.townhalls.amount * 22 and nexus.is_idle:
+            if self.can_afford(UnitTypeId.PROBE):
+                nexus.train(UnitTypeId.PROBE)
 
-    async def build_pylons(self):
-        if self.units(NEXUS).amount > 0:
-            nexus = self.units(NEXUS).random
+    async def build_pylons(self, nexus):
+        if self.supply_used < 50:
+            if self.supply_left < 3 and not self.already_pending(UnitTypeId.PYLON):
+                await self.build(UnitTypeId.PYLON, near=nexus, max_distance=40)
         else:
-            return
-        if not nexus.shield_max:
-            return
-        if self.supply_used < 60:
-            if self.supply_left < 5 and not self.already_pending(PYLON):
-                await self.build(PYLON, near=nexus, max_distance=40, unit=self.workers.random)
-        else:
-            if self.supply_left < 7 and self.units(PYLON).not_ready.amount < 2:
-                await self.build(PYLON, near=nexus, max_distance=40, unit=self.workers.random)
+            if self.supply_left < 6 and self.units(UnitTypeId.PYLON).not_ready.amount < 2:
+                await self.build(UnitTypeId.PYLON, near=nexus, max_distance=40)
 
     async def build_assimilators(self):
-        for nexus in self.units(NEXUS).ready:
+        for nexus in self.units(UnitTypeId.NEXUS).ready:
             vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
             if self.units(ASSIMILATOR).amount >= self.units(NEXUS).amount*2:
                 break
             for vaspene in vaspenes:
-                if not self.can_afford(ASSIMILATOR):
+                if not self.can_afford(UnitTypeId.ASSIMILATOR):
                     break
                 worker = self.select_build_worker(vaspene.position)
                 if worker is None:
                     break
-                if (not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists) and self.units(PYLON).amount > 0:
-                    await self.do(worker.build(ASSIMILATOR, vaspene))
+                if (not self.units(UnitTypeId.ASSIMILATOR).closer_than(1.0, vaspene).exists) and self.units(UnitTypeId.PYLON).amount > 0:
+                    await self.do(worker.build(UnitTypeId.ASSIMILATOR, vaspene))
 
     async def expand(self):
-        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+        if self.units(UnitTypeId.NEXUS).amount < 3 and self.can_afford(UnitTypeId.NEXUS):
             await self.expand_now()
-        if len(self.workers.idle) > 10 and self.can_afford(NEXUS) and not self.already_pending(NEXUS):
+        if len(self.workers.idle) > 10 and self.can_afford(UnitTypeId.NEXUS) and not self.already_pending(UnitTypeId.NEXUS):
             await self.expand_now()
 
     async def handle_chronoboost(self, nexus):
@@ -96,43 +107,42 @@ class SigmaStar(sc2.BotAI):
                 await self.do(nexus(EFFECT_CHRONOBOOSTENERGYCOST, nexus))
 
     async def offensive_force_buildings(self):
-        pylon = self.units(PYLON).ready.random
-        if self.units(PYLON).ready.exists:
-            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE).exists:
-                await self.build(CYBERNETICSCORE, near=pylon)
-                return
-            if self.units(GATEWAY).ready.exists and not self.units(FORGE).exists:
-                await self.build(FORGE, near=pylon)
-                return
-            if self.units(GATEWAY).amount < self.units(NEXUS).amount - 1:
-                await self.build(GATEWAY, near=pylon)
-                return
-            if self.units(CYBERNETICSCORE).ready.exists and not self.units(TWILIGHTCOUNCIL).exists:
-                await self.build(TWILIGHTCOUNCIL, near=pylon)
-                return
-            if self.units(CYBERNETICSCORE).ready.exists:
-                if self.units(STARGATE).amount < self.units(NEXUS).amount:
-                    if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
-                        await self.build(STARGATE, near=pylon)
-                if self.units(ROBOTICSFACILITY).amount < 1 and self.units(STARGATE).ready.exists:
-                    if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
-                        await self.build(ROBOTICSFACILITY, near=pylon)
+        pylon = self.structures(UnitTypeId.PYLON).ready.random
+        if pylon:
+            # if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE).exists:
+            #     await self.build(CYBERNETICSCORE, near=pylon)
+            #     return
+            # if self.units(GATEWAY).ready.exists and not self.units(FORGE).exists:
+            #     await self.build(FORGE, near=pylon)
+            #     return
+            if self.structures(UnitTypeId.GATEWAY).amount < self.structures(UnitTypeId.NEXUS).amount:
+                await self.build(UnitTypeId.GATEWAY, near=pylon)
+            # if self.units(CYBERNETICSCORE).ready.exists and not self.units(TWILIGHTCOUNCIL).exists:
+            #     await self.build(TWILIGHTCOUNCIL, near=pylon)
+            #     return
+            # if self.units(CYBERNETICSCORE).ready.exists:
+            #     if self.units(STARGATE).amount < self.units(NEXUS).amount:
+            #         if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
+            #             await self.build(STARGATE, near=pylon)
+            #     if self.units(ROBOTICSFACILITY).amount < 1 and self.units(STARGATE).ready.exists:
+            #         if self.can_afford(ROBOTICSFACILITY) and not self.already_pending(ROBOTICSFACILITY):
+            #             await self.build(ROBOTICSFACILITY, near=pylon)
 
     async def build_offensive_force(self):
-        for sg in self.units(STARGATE).ready.idle:
-            if self.can_afford(VOIDRAY) and self.supply_left > 2:
-                await self.do(sg.train(VOIDRAY))
-        for gate in self.units(GATEWAY).ready.idle:
-            if self.can_afford(STALKER) and self.supply_left > 2 and self.units(STALKER).amount < self.units(VOIDRAY).amount + 2:
-                await self.do(gate.train(STALKER))
-            if self.can_afford(ZEALOT) and self.supply_left > 1 and self.units(ZEALOT).amount < self.units(NEXUS).amount + 1:
-                await self.do(gate.train(ZEALOT))
-            if self.can_afford(ADEPT) and self.supply_left > 1 and self.units(ADEPT).amount < self.units(ZEALOT).amount\
-                    and self.units(CYBERNETICSCORE).ready.exists:
-                await self.do(gate.train(ADEPT))
-        for rg in self.units(ROBOTICSFACILITY).ready.idle:
-            if self.can_afford(OBSERVER) and self.supply_left > 0 and not self.units(OBSERVER).exists:
-                await self.do(rg.train(OBSERVER))
+        # for sg in self.units(STARGATE).ready.idle:
+        #     if self.can_afford(VOIDRAY) and self.supply_left > 2:
+        #         await self.do(sg.train(VOIDRAY))
+        for gate in self.structures(UnitTypeId.GATEWAY).ready.idle:
+            # if self.can_afford(STALKER) and self.supply_left > 2 and self.units(STALKER).amount < self.units(VOIDRAY).amount + 2:
+            #     await self.do(gate.train(STALKER))
+            if self.can_afford(UnitTypeId.ZEALOT) and self.supply_left > 1 and self.supply_army < 100:
+                gate.train(UnitTypeId.ZEALOT)
+            # if self.can_afford(ADEPT) and self.supply_left > 1 and self.units(ADEPT).amount < self.units(ZEALOT).amount\
+            #         and self.units(CYBERNETICSCORE).ready.exists:
+            #     await self.do(gate.train(ADEPT))
+        # for rg in self.units(ROBOTICSFACILITY).ready.idle:
+        #     if self.can_afford(OBSERVER) and self.supply_left > 0 and not self.units(OBSERVER).exists:
+        #         await self.do(rg.train(OBSERVER))
 
     async def handle_upgrades(self):
         if self.units(TWILIGHTCOUNCIL).ready.exists:
@@ -184,17 +194,14 @@ class SigmaStar(sc2.BotAI):
                 return
 
     async def attack(self):
-        army_units = self.units(STALKER).ready | self.units(ZEALOT).ready | self.units(ADEPT).ready | self.units(
-            VOIDRAY).ready | self.units(OBSERVER).ready
+        army_units = self.units(UnitTypeId.ZEALOT).ready
+        targets = (self.enemy_units | self.enemy_structures).filter(lambda unit: unit.can_be_attacked)
         for a in army_units.idle:
-            if len(self.known_enemy_units) > 0:
-                await self.do(a.attack(random.choice(self.known_enemy_units).position))
-            elif self.units(STALKER).amount > 12 or self.supply_used > 150:
-                # await self.do(a.attack(self.find_target(self, a)))
-                if self.known_enemy_units.closer_than(20, self.enemy_start_locations[0]).amount > 0:
-                    await self.do(a.attack(self.enemy_start_locations[0]))
-                else:
-                    await self.do(a.attack(random.choice(self.enemy_start_locations)))
+            if targets:
+                target = targets.closest_to(a)
+                a.attack(target)
+            elif self.supply_used > 150:
+                a.attack(self.enemy_start_locations[0])
 
     # def find_target(self, unit):
     #     if len(self.known_enemy_units) > 0:
@@ -216,13 +223,13 @@ class SigmaStar(sc2.BotAI):
 
 
 def main():
-    run_game(maps.get("TritonLE"),
-             [Bot(Race.Protoss, SigmaStar()),
-             Computer(Race.Protoss, Difficulty.Hard)],
+    run_game(maps.get("AcropolisLE"),
+             [Bot(Race.Protoss, KhalaiStar()),
+             Computer(Race.Protoss, Difficulty.Medium)],
              realtime=False)
-    # run_game(maps.get("TritonLE"), [
+    # run_game(maps.get("AcropolisLE"), [
     #         Human(Race.Zerg),
-    #         Bot(Race.Protoss, SigmaStar())],
+    #         Bot(Race.Protoss, KhalaiStar())],
     #         realtime=True, save_replay_as="Protoss vs Protoss.SC2Replay")
 
 
